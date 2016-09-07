5f09fd22e9c2224b61ed8bddf155577eb2bfbf7b
savreline <savreline@gmail.com>
2016-09-07 13:23:54 -0700

    Update heateq.m
    Sept 7 version with code written as per wiki convention

--- issue_01/heateq.m
+++ issue_01/heateq.m
@@ -1,37 +1,49 @@
-function dy = heateq(t, y) % so y is solution function  
+function dT = heateq(t, T) 
+global N 
 
-Tair = 298;
-z = 1;
-k = 450000;
+% A - Nitrogen Data
+Cp_a = 1039; 
+T_a_in = 80;
+rho_a = 1000;
+F_a = 0.2;
 
-% Shell Side
-Cps = 4185;
-T0s = 250;
-As = 0.02543;
-ps = 1000;
-Fs = 0.1;
+% B - Helium Data
+Cp_b = 5190;
+T_b_in = 4;
+rho_b = 1030;
+F_b = 0.2;
 
-% Tube Side
-Cpt = 1200;
-T0t = 330;
-At = 0.0314;
-pt = 1030;
-Ft = 0.2;
+% Area Information
+diam = 0.0254;
+Ac = pi*(diam/2)^2;
+HX_L = 1;
 
-mt = pt * At * z;
-ms = ps * As * z;
+% Heat Transfer Coefficients 
+HX_UA_a = 2000;
+HX_UA_b = 2000; 
 
-dy = zeros(2, 1);  
-N  =  3;
-y(1) = T0t;
-y(2 * N + 2) = T0s;
+% Compute Masses
+m_a = rho_a*Ac*HX_L;
+m_b = rho_b*Ac*HX_L;
 
+% Prepare to solve - preallocate dummy vector
+dT = zeros(2, 1);  
+% Prepare to solve - enter initial data into the DE 
+T(1) = T_a_in;
+T(N + 2) = T_b_in;
+
+% A - Nitrogen DE
 for i = 2 : N + 1
-    dy(i) = (pt * Ft * y(i - 1) - pt * Ft * y(i) - k * At / z * (y(i) - y(i + N + 1))) / (Cpt * mt);
+    Q_in_a = rho_a * F_a * T(i - 1); 
+    Q_out_a = rho_a * F_a * T(i);
+    dT(i) = (Q_in_a - Q_out_a - HX_UA_a) / (Cp_a * m_a);
 end
 
-for i = N + 2 : 2 * N + 1
-    dy(i) = (ps * Fs * y(i) - ps * Fs * y(i + 1) - k * As / z * (y(i) - y(i - N - 1))) / (Cps * ms);
+% B - Helium DE
+for i = N + 3 : 2 * N + 2
+    Q_in_b = rho_b * F_b * T(i - 1);
+    Q_out_b = rho_b * F_b * T(i);
+    dT(i) = (Q_in_b - Q_out_b + HX_UA_b) / (Cp_b * m_b);
 end
 
 return
