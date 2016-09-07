92d3ec533bfd64ba904d7b83c0709102c3a423c1
savreline <savreline@gmail.com>
2016-09-07 13:25:16 -0700

    As per wiki convention
    Sept 7 version with code written as per wiki convention

--- issue_01/solver.m
+++ issue_01/solver.m
@@ -1,36 +1,47 @@
 close all
 clear all
 clc
+global N 
 
+%How many stages?
 N = 3; 
-tint = [0 10];
-y0(1:N+1)=330;
-y0(N+2:2*N+1)=250; 								% initial conditions
+% What is our time interval?
+t_interval = [0 10];
+% Initial conditions 
+T0(1:N+1)=80;
+T0(N+2:2*N+2)=4; 							
 
-options = odeset('RelTol',1e-9,'AbsTol',[1e-9*ones(1,2*N+1)]);
+% Some technical options for ode45 - to ensure accuracy 
+options = odeset('RelTol',1e-9,'AbsTol',[1e-9*ones(1,2*N+2)]);
+% Solve 
+% The solver produces matrix T with solution data. 
+% Each row contains T at each stage of the heat exchanger, for a fixed t.
+% For N=3, columns 1-4 contain stream A, columns 5-8 contain stream B. 
+[t, T] = ode45('heateq', t_interval, T0, options);
 
-[t, y] = ode45('heateq', tint, y0, options);
-
+% ******************
+% Plot T vs time 
 hold on
-plot(t,y(:,2),'-r'); xlabel('Time'); ylabel('Temp');
-plot(t,y(:,3),'--r'); xlabel('Time'); ylabel('Temp');
-plot(t,y(:,4),'-.r'); xlabel('Time'); ylabel('Temp');
-plot(t,y(:,5),'-.k'); xlabel('Time'); ylabel('Temp');
-plot(t,y(:,6),'--k'); xlabel('Time'); ylabel('Temp');
-plot(t,y(:,7),'-k'); xlabel('Time'); ylabel('Temp');
-legend('Tube 1','Tube 2','Tube 3','Shell 3','Shell 2','Shell 1')
+plot(t,T(:,2),'-r','LineWidth',2); xlabel('Time'); ylabel('Temp');
+plot(t,T(:,3),'--r','LineWidth',2); xlabel('Time'); ylabel('Temp');
+plot(t,T(:,4),'-.r','LineWidth',2); xlabel('Time'); ylabel('Temp');
+plot(t,T(:,6),'-.b','LineWidth',2); xlabel('Time'); ylabel('Temp');
+plot(t,T(:,7),'--b','LineWidth',2); xlabel('Time'); ylabel('Temp');
+plot(t,T(:,8),'-b','LineWidth',2); xlabel('Time'); ylabel('Temp');
+legend('N2 Stage 1','N2 Stage 2','N2 Stage 3','He Stage 3','He Stage 2','He Stage 1','location','eastoutside')
 fig = gcf;
 fig.PaperPositionMode = 'auto';
-print('plot3','-dpng','-r0')
+print('plot_T_vs_time','-dpng','-r0')
 
-
-%plot(t,y(:,2),'-r'); xlabel('Time'); ylabel('Temp');
-%plot(t,y(:,3),'--r'); xlabel('Time'); ylabel('Temp');
-%plot(t,y(:,4),'-k'); xlabel('Time'); ylabel('Temp');
-%plot(t,y(:,5),'--k'); xlabel('Time'); ylabel('Temp');
-%legend('Tube 1','Tube 2','Shell 2','Shell 1')
-%fig = gcf;
-%fig.PaperPositionMode = 'auto';
-%print('plot2','-dpng','-r0')
-
-
+% ******************
+% Plot T vs length of heat exchanger stages  
+figure 
+hold on
+a = round(length(t)/2); %find the middle of time vector
+plot(1:4,T(a,1:4),'-r','LineWidth',2); xlabel('Stage'); ylabel('Temp');
+plot(1:4,fliplr(T(a,5:8)),'-b','LineWidth',2); xlabel('Stage'); ylabel('Temp');
+%we need to flip the He vector since we are counter-current
+legend('Nitrogen','Helium','location','eastoutside')
+fig = gcf;
+fig.PaperPositionMode = 'auto';
+print('plot_T_vs_length','-dpng','-r0')
