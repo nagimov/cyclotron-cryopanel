c9f2db48025a975f0fc240a8d1b151e0604a4224
savreline <savreline@gmail.com>
2016-09-08 14:46:40 -0700

    Remove assump. HX_UA constant, some housekeeping

--- issue_08/solver.m
+++ issue_08/solver.m
@@ -1,17 +1,23 @@
 clc
 clear all
-global t N 
+global t mid last 
 
 t = 5; %How many time steps do we take? (time step be specified, currently 0.1s)
 N = 3; %How many stages?
 
+%Calculate indicies
+mid = N + 2;               %where He columns start
+last = 2 * (N +  1);       %last column
+
 %Take an intial guess 
-T0a = 80 * ones ( t + 1 , N + 1 );
-T0b = 4.5 * ones ( t + 1 , N + 1 ); 
-T0 = [T0a T0b ];
+T0a = 80 * ones ( t + 1 , mid - 1 );
+T0b = 4 * ones ( t + 1 , mid - 1 ); 
+T0 = [T0a T0b];
 
 %Solve 
-T = fsolve ( 'heateq' , T0 ); 
+options = optimset('TolX', 1e-3, 'TolFun', 1e-3, ...
+    'MaxFunEvals', 1e7, 'MaxIter', 1e7, 'Display', 'off');
+T = fsolve ( 'heateq' , T0 , options ); 
 
 %Plot
 hold on
@@ -28,8 +34,8 @@ print('plot_T_vs_time','-dpng','-r0')
 
 figure 
 hold on
-plot(1:N+1,T(6,1:N+1),'-r','LineWidth',2); xlabel('Stage'); ylabel('Temp');
-plot(1:N+1,fliplr(T(6,N+2:2*(N+1))),'-b','LineWidth',2); xlabel('Stage'); ylabel('Temp');
+plot(1:mid-1,T(6,1:mid-1),'-r','LineWidth',2); xlabel('Stage'); ylabel('Temp');
+plot(1:mid-1,fliplr(T(6,mid:last)),'-b','LineWidth',2); xlabel('Stage'); ylabel('Temp');
 %we need to flip the He vector since we are counter-current
 legend('Nitrogen','Helium','location','eastoutside')
 fig = gcf;
