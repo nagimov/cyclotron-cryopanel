7b3b45c4d47f07540e20e8c438934470d6dd9748
savreline <savreline@gmail.com>
2016-09-08 15:53:53 -0700

    Added penalty functions for boundary conditions

--- issue_08/heateq.m
+++ issue_08/heateq.m
@@ -16,29 +16,24 @@ Ac = pi * rad^2;
 As = 2 * pi * rad * HX_L;
 
 % Heat Transfer Coefficients 
-k_a = 5e6;
-k_b = 5e6; 
+k_a = 5e4;
+k_b = 5e4; 
 
-%Set initial data
-T(1, 1 : mid - 1 ) = 80; 
-T(1, mid : last ) = 4; 
-T(:, 1) = 80;
-T(:, mid) = 4; 
 %Get U for this data 
-U_a(1) = refpropm('U','T',T(1,1),'Q',.75,'nitrogen');
-U_b(1) = refpropm('U','T',T(1,mid),'P',101,'helium');
+U_a(1,1) = refpropm('U','T',T(1,1),'Q',.75,'nitrogen');
+U_b(1,1) = refpropm('U','T',T(1,mid),'P',101,'helium');
 
 %Equations Nitrogen 
 for j = 2 : mid - 1 %stages
 for i = 2 : t + 1 %time step 
 
 delta_T = T(i, last - j + 1 ) - T(i,j);
-U_a(i) = refpropm('U','T',T(i,j),'Q',.75,'nitrogen');
-H_a_in = refpropm('H','T',T(i,j-1),'Q',.75,'nitrogen');
-H_a_out = refpropm('H','T',T(i,j),'Q',.75,'nitrogen');
+U_a(i,j) = refpropm('U','T',T(i,j),'Q',.75,'nitrogen');
+H_a_in(i,j) = refpropm('H','T',T(i,j-1),'Q',.75,'nitrogen');
+H_a_out(i,j) = refpropm('H','T',T(i,j),'Q',.75,'nitrogen');
 Q_cond_a = k_a * As / HX_L * delta_T ;
 
-F(i,j) = U_a(i) - U_a(i-1) + delta_t * ( m * H_a_in - m * H_a_out + Q_cond_a ) / M; 
+F(i,j) = U_a(i,j) - U_a(i-1,j) + delta_t * ( m * H_a_in(i,j) - m * H_a_out(i,j) + Q_cond_a ) / M; 
 
 end 
 end 
@@ -47,14 +42,31 @@ end
 for j = mid + 1 : last %stages
 for i = 2 : t + 1  %time step 
 
-delta_T = T(i, last - j + 1 ) - T(i,j);
-U_b(i) = refpropm('U','T',T(i,j),'P',101,'helium');
-H_b_in = refpropm('H','T',T(i,j-1),'P',101,'helium');
-H_b_out = refpropm('H','T',T(i,j),'P',101,'helium');
+delta_T = T(i, last - j + 1 ) - T(i,j); 
+U_b(i,j) = refpropm('U','T',T(i,j),'P',101,'helium');
+H_b_in(i,j) = refpropm('H','T',T(i,j-1),'P',101,'helium');
+H_b_out(i,j) = refpropm('H','T',T(i,j),'P',101,'helium');
 Q_cond_b = k_b * As / HX_L * delta_T;
 
-F(i,j) = U_b(i) - U_b(i-1) + delta_t * ( m * H_b_in - m * H_b_out + Q_cond_b ) / M; 
+F(i,j) = U_b(i,j) - U_b(i-1,j) + delta_t * ( m * H_b_in(i,j) - m * H_b_out(i,j) + Q_cond_b ) / M; 
+
 
 end 
 end
 
+%Ensure boundary conditions
+for j = 1 : mid - 1
+F(1, j) = 1e8*(80 - T(1,j));
+end
+
+for j = mid : last 
+F(1, j) = 1e8*(4 - T(1,j)); 
+end
+
+for i = 1 : t + 1
+F(i, 1) = 1e8*(80 - T(i,1));
+end
+
+for i = 1 : t + 1
+F(i, mid) = 1e8*(4 - T(i, mid));
+end
