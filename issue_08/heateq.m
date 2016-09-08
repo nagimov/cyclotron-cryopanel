03a682f62e6be36d9e4fdbdc983d6add76d7e558
savreline <savreline@gmail.com>
2016-09-08 14:46:09 -0700

    Remove assump. HX_UA constant, some housekeeping

--- issue_08/heateq.m
+++ issue_08/heateq.m
@@ -1,45 +1,59 @@
 function F = heateq(T)  %Here F compute difference between our equation and zero i.e. F(T)~0. 
-global t N
+global t mid last 
 
 %Time Step
-delta_t = 0.1; 
+delta_t = .1;
 
 %Data
-m = 1000; 
-HX_UA_a = 6e6; %For now assume fixed flux
-HX_UA_b = 6e6; %For now assume fixed flux
+m = 1; 
+M = 100; 
+
+% Area Information
+diam = 0.0254;
+rad = diam/2;
+HX_L = 1;
+Ac = pi * rad^2;
+As = 2 * pi * rad * HX_L;
+
+% Heat Transfer Coefficients 
+k_a = 5e6;
+k_b = 5e6; 
 
 %Set initial data
-T(1, 1 : N + 1 ) = 80; 
-T(1, N + 2 : 2 * ( N + 1 )) = 4.5; 
+T(1, 1 : mid - 1 ) = 80; 
+T(1, mid : last ) = 4; 
 T(:, 1) = 80;
-T(:, N + 2) = 4.5; 
+T(:, mid) = 4; 
 %Get U for this data 
-U_a(1) = refpropm('U','T',T(1,1),'P',101,'nitrogen');
-U_b(1) = refpropm('U','T',T(1,N+2),'P',101,'helium');
+U_a(1) = refpropm('U','T',T(1,1),'Q',.75,'nitrogen');
+U_b(1) = refpropm('U','T',T(1,mid),'P',101,'helium');
 
 %Equations Nitrogen 
-for j = 2 : N + 1 %stages
-for i = 1 : t + 1 %time step 
-    
-U_a(i+1) = refpropm('U','T',T(i,j),'P',101,'nitrogen');
-H_a_in = refpropm('H','T',T(i,j-1),'P',101,'nitrogen');
-H_a_out = refpropm('H','T',T(i,j),'P',101,'nitrogen');
+for j = 2 : mid - 1 %stages
+for i = 2 : t + 1 %time step 
 
-F(i+1,j) = U_a(i+1) - U_a(i) + delta_t * ( m * H_a_in - m * H_a_out - HX_UA_a ); 
+delta_T = T(i, last - j + 1 ) - T(i,j);
+U_a(i) = refpropm('U','T',T(i,j),'Q',.75,'nitrogen');
+H_a_in = refpropm('H','T',T(i,j-1),'Q',.75,'nitrogen');
+H_a_out = refpropm('H','T',T(i,j),'Q',.75,'nitrogen');
+Q_cond_a = k_a * As / HX_L * delta_T ;
+
+F(i,j) = U_a(i) - U_a(i-1) + delta_t * ( m * H_a_in - m * H_a_out + Q_cond_a ) / M; 
 
 end 
 end 
 
 %Equations Helium  
-for j = N + 3 : 2*(N + 1) %stages
-for i = 1 : t + 1  %time step 
-    
-U_b(i+1) = refpropm('U','T',T(i,j),'P',101,'helium');
+for j = mid + 1 : last %stages
+for i = 2 : t + 1  %time step 
+
+delta_T = T(i, last - j + 1 ) - T(i,j);
+U_b(i) = refpropm('U','T',T(i,j),'P',101,'helium');
 H_b_in = refpropm('H','T',T(i,j-1),'P',101,'helium');
 H_b_out = refpropm('H','T',T(i,j),'P',101,'helium');
+Q_cond_b = k_b * As / HX_L * delta_T;
 
-F(i+1,j) = U_b(i+1) - U_b(i) + delta_t * ( m * H_b_in - m * H_b_out + HX_UA_b ); 
+F(i,j) = U_b(i) - U_b(i-1) + delta_t * ( m * H_b_in - m * H_b_out + Q_cond_b ) / M; 
 
 end 
 end
