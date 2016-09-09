da3f80b354b49ea51964252d425a90fc25d642ad
savreline <savreline@gmail.com>
2016-09-09 16:22:55 -0700

    Code using fmincon, constrained optimization
    changed U and H to lower case

--- issue_08/heateq.m
+++ issue_08/heateq.m
@@ -5,7 +5,7 @@ global t mid last
 delta_t = .001;
 
 %Data
-m = 1; 
+m = 1000; 
 M = 1; 
 
 % Area Information
@@ -22,20 +22,20 @@ k_a = 1e3;
 k_b = 1e3; 
 
 %Get U for this data 
-U_a(1,1) = refpropm('U','T',T(1,1),'Q',q_a,'nitrogen');
-U_b(1,1) = refpropm('U','T',T(1,mid),'P',p_a,'helium');
+u_a(1,1) = refpropm('U','T',T(1,1),'Q',q_a,'nitrogen');
+u_b(1,1) = refpropm('U','T',T(1,mid),'P',p_a,'helium');
 
 %Equations Nitrogen 
 for j = 2 : mid - 1 %slices
 for i = 2 : t + 1 %time step 
 
 delta_T = T(i, last - j + 1 ) - T(i,j);
-U_a(i,j) = refpropm('U','T',T(i,j),'Q',q_a,'nitrogen');
-H_a_in(i,j) = refpropm('H','T',T(i,j-1),'Q',q_a,'nitrogen');
-H_a_out(i,j) = refpropm('H','T',T(i,j),'Q',q_a,'nitrogen');
+u_a(i,j) = refpropm('U','T',T(i,j),'Q',q_a,'nitrogen');
+h_a_in(i,j) = refpropm('H','T',T(i,j-1),'Q',q_a,'nitrogen');
+h_a_out(i,j) = refpropm('H','T',T(i,j),'Q',q_a,'nitrogen');
 Q_cond_a = k_a * As / HX_L * delta_T ;
 
-F(i,j) = U_a(i-1,j) - U_a(i,j) + delta_t / M * ( m * H_a_in(i,j) - m * H_a_out(i,j) + Q_cond_a ); 
+F(i,j) = u_a(i-1,j) - u_a(i,j) + delta_t / M * ( m * h_a_in(i,j) - m * h_a_out(i,j) + Q_cond_a ); 
 
 end 
 end 
@@ -45,12 +45,12 @@ for j = mid + 1 : last %slices
 for i = 2 : t + 1  %time step 
 
 delta_T = T(i, last - j + 1 ) - T(i,j); 
-U_b(i,j) = refpropm('U','T',T(i,j),'P',p_a,'helium');
-H_b_in(i,j) = refpropm('H','T',T(i,j-1),'P',p_a,'helium');
-H_b_out(i,j) = refpropm('H','T',T(i,j),'P',p_a,'helium');
+u_b(i,j) = refpropm('U','T',T(i,j),'P',p_a,'helium');
+h_b_in(i,j) = refpropm('H','T',T(i,j-1),'P',p_a,'helium');
+h_b_out(i,j) = refpropm('H','T',T(i,j),'P',p_a,'helium');
 Q_cond_b = k_b * As / HX_L * delta_T;
 
-F(i,j) = U_b(i-1,j) - U_b(i,j) + delta_t / M * ( m * H_b_in(i,j) - m * H_b_out(i,j) + Q_cond_b ); 
+F(i,j) = u_b(i-1,j) - u_b(i,j) + delta_t / M * ( m * h_b_in(i,j) - m * h_b_out(i,j) + Q_cond_b ); 
 
 end 
 end
