4974193d94581446e038f0dc74bf7b5a77d1f129
savreline <savreline@gmail.com>
2016-09-26 16:12:31 -0700

    easier way to write this wrapper

--- issue_09/refpropwrap.m
+++ issue_09/refpropwrap.m
@@ -1,15 +1,7 @@
-function fval =  refpropwrap(Name,Prop,T,P) 
+function fval =  refpropwrap(Prop,T,P,Name) 
 
-a = strcmp(Name,'nitrogen'); %Check to see if input matches nitrogen 
-b = strcmp(Name,'helium'); %Check to see if input matches helium 
-
-if a == 1 && T <= 63.151 %If a was indeed a positive comparison, then check limit for nitrogen 
-    fval = 1e10;
-elseif b == 1 && T <= 2.1768 %If b was indeed a positive comparison, then check limit for helium 
-    fval = 1e10;
-else 
+try
     fval = refpropm(Prop,'T',T,'P',P,Name); %If all limits are OK, just evaluate  
+catch
+    fval = 1e10; 
 end 
-
-
-
