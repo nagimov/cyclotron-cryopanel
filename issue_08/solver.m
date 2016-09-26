a98f1700b76c9b2ef21b6fc641b5ca0e2ea5b3a4
savreline <savreline@gmail.com>
2016-09-26 15:12:30 -0700

    Refprop wrapper introduced  + minor edits

--- issue_08/solver.m
+++ issue_08/solver.m
@@ -1,5 +1,6 @@
 clc
 clear all
+close all 
 global mid last Tsol delta_t 
 
 delta_t = .01; %Time Step
@@ -18,8 +19,24 @@ Tsol(1, :) = [T0a T0b];
 %Solve 
 for j = 2 : t + 1
 [Tsol(j, :),fval(j,:),exitflag(j,:)] = heateq(j); 
+clc
+disp(['Time step completed ' num2str(j)])
 end 
 
+%Plot 1 
 hold on
 plot(1:11,Tsol(:,1:mid-1))
 plot(1:11,Tsol(:,mid:last))
+fig = gcf;
+fig.PaperPositionMode = 'auto';
+print('plot_T_vs_length','-dpng','-r0')
+
+%Plot 2
+figure
+hold on 
+plot(1:20,Tsol(1:20,1:mid-1))
+plot(1:20,Tsol(1:20,mid:last))
+fig = gcf;
+fig.PaperPositionMode = 'auto';
+print('plot_T_vs_time','-dpng','-r0'
+
