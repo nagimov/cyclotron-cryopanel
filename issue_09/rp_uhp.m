0121e1385edd040293d0bb553e98d6d1534f6046
savreline <savreline@gmail.com>
2016-09-28 14:16:34 -0700

    Create rp_uhp.m

---
+++ issue_09/rp_uhp.m
@@ -0,0 +1,35 @@
+function f =  rp_uhp(h,p,Name) 
+
+p = p/1000;
+
+try
+    f = refpropm('U','H',h,'P',p,Name);  
+catch ME 
+    %Pick out numbers from error message
+    val = regexp(ME.message, '[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?','match');
+    
+    if str2double(val{1}) == 249 %Code 249 means the error is on H limits  
+    h_min = str2double(val{3}); %Get h_min value 
+    h_max = str2double(val{4}); %Get h_max value 
+    
+    if h < h_min
+      f = rp_uhp(h_min,p,Name);
+      display('using h_min value!')
+    else
+      f = rp_uhp(h_max,p,Name);
+      display('using h_max value!')
+    end
+    
+    elseif str2double(val{1}) == 4 && p > 0 %Code 4 means the error is on the upper P limit
+      p_max = str2double(val{4}); %Get p_max value 
+      f = rp_uhp(h,p_max,Name);
+      display('using p_max value!')
+    elseif p < 0
+      p_min = 0;  
+      f = rp_uhp(h,p_min,Name);
+      display('using p_min value!')  
+    else   
+      disp(ME.message)
+      f = 'unknown error, see above msg'; 
+    end
+end 
