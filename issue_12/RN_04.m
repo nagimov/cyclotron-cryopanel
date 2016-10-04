8f888923620d80adf0343a3ba86c50912302053c
savreline <savreline@gmail.com>
2016-10-04 11:33:49 -0700

    Code split into blocks

--- issue_12/RN_04.m
+++ issue_12/RN_04.m
@@ -1,19 +1,19 @@
-function [T_a_sol, T_b_sol, T_w_sol] = RN_04
+function [T_a_sol, T_b_sol, T_w_sol] = RN_04 %With wall 
     clc; clear all;
     close all;
     tic
 
     % INTEGRATOR DATA
     t_delta = 0.1;  % time step, s
-    t = 20;  % number of time steps, -
+    t = 5;  % number of time steps, -
     HX_slices = 10;  % number of slices, -
 
     % HX DATA
     m = 1;  % mass flow, kg/s
     M = 1;  % mass of streams content within a cell, kg
     M_w = 1; % mass of wall, kg 
-    HX_UA_aw = 10000;  % HX coefficient, stream A to wall, W/K
-    HX_UA_bw = 10000;  % HX coefficient, stream B to wall, W/K
+    HX_UA_data = {'nitrogen', 10000; ...
+                    'helium', 10000}; % HX coefficient, W/K
 
     % INITIAL DATA
     fluid_a = 'helium';  % stream A fluid name
@@ -48,6 +48,24 @@ function [T_a_sol, T_b_sol, T_w_sol] = RN_04
         T_w_sol(j, :) = sol(2 * HX_slices + 1 : 3 * HX_slices );
     end
     
+    % FUNCTION THAT PULLS HX_UA DATA
+    function value = HX_UA(fluid)        
+        f = strcmp(fluid, HX_UA_data);   
+        value = HX_UA_data{f, 2};
+    end
+
+    % FUNCTION THAT CALCULATES Q
+    function Q_cond = Q_cond(h, p, T_w, fluid)
+        T = rp_thp(h, p, fluid);
+        T_delta = T_w - T;
+        Q_cond = HX_UA(fluid) / HX_slices * T_delta;
+    end 
+
+    % FUNCTION TAHT COMPUTES delta_u
+    function delta_u = u_delta(h, h_prev, p, fluid)
+		delta_u = rp_uhp(h, p, fluid) - rp_uhp(h_prev, p, fluid); 
+    end
+        
     %CONVERT TO T    
     T_a_sol = zeros(t + 1, HX_slices);
     T_b_sol = zeros(t + 1, HX_slices);
@@ -57,8 +75,8 @@ function [T_a_sol, T_b_sol, T_w_sol] = RN_04
             T_a_sol(i,j) = rp_thp(h_a_sol(i,j), p_a_in, fluid_a);
             T_b_sol(i,j) = rp_thp(h_b_sol(i,j), p_b_in, fluid_b);
         end
-    end  
-    
+    end
+     
     % HE PLOT 
     plot(1:HX_slices, T_a_sol)
     xlabel('Slices')
@@ -110,18 +128,13 @@ function [T_a_sol, T_b_sol, T_w_sol] = RN_04
 	    	F_a = zeros(1, HX_slices);
 	    	F_b = zeros(1, HX_slices);
    	    	F_w = zeros(1, HX_slices);
-	    	u_b = zeros(1, HX_slices);
-	    	u_a = zeros(1, HX_slices);
-	    	u_b_prev = zeros(1, HX_slices);
-	    	u_a_prev = zeros(1, HX_slices);
+	    	u_a_delta = zeros(1, HX_slices);
+	    	u_b_delta = zeros(1, HX_slices);
 			h_a_delta = zeros(1, HX_slices);
 			h_b_delta = zeros(1, HX_slices);
 			Q_cond_aw = zeros(1, HX_slices);
 			Q_cond_bw = zeros(1, HX_slices);
-            T_a = zeros(1, HX_slices);
-			T_b = zeros(1, HX_slices);
-			T_aw_delta = zeros(1, HX_slices);
-			T_bw_delta = zeros(1, HX_slices);
+            T_w_delta = zeros(1, HX_slices); 
             cp_w = zeros(1, HX_slices); 
             
             %split solution vector 
@@ -132,26 +145,17 @@ function [T_a_sol, T_b_sol, T_w_sol] = RN_04
             p_b = p_b_0;
 
 			% energy equations
-			for i = 1 : HX_slices  % slicing loop
-				T_a(i) = rp_thp(h_a(i), p_a(i), fluid_a);
-				T_b(i) = rp_thp(h_b(i), p_b(i), fluid_b);
-
-                T_aw_delta(i) = T_w(i) - T_a(i);
-				T_bw_delta(i) = T_w(i) - T_b(i);
-				
-				Q_cond_aw(i) = HX_UA_aw / HX_slices * T_aw_delta(i);
-    			Q_cond_bw(i) = HX_UA_bw / HX_slices * T_bw_delta(i);
-
+			for i = 1 : HX_slices  % slicing loop                
+                Q_cond_aw(i) = Q_cond(h_a(i), p_a(i), T_w(i), fluid_a);
+                Q_cond_bw(i) = Q_cond(h_b(i), p_b(i), T_w(i), fluid_b);
+                u_a_delta(i) = u_delta(h_a(i), h_a_prev(i), p_a(i), fluid_a);
+                u_b_delta(i) = u_delta(h_b(i), h_b_prev(i), p_b(i), fluid_b);
+                T_w_delta(i) = T_w(i) - T_w_prev(i); 
                 cp_w(i) = cp(T_w(i)); 
-                
-				u_a(i) = rp_uhp(h_a(i), p_a(i), fluid_a);
-				u_a_prev(i) = rp_uhp(h_a_prev(i), p_a(i), fluid_a);
-				u_b(i) = rp_uhp(h_b(i), p_b(i), fluid_b);   
-     			u_b_prev(i) = rp_uhp(h_b_prev(i), p_b(i), fluid_b);             
-           
+                        
             % enthalpy deltas  
     			if i == 1
-					h_a_delta(i) = h_a_in - h_a(1);
+					h_a_delta(1) = h_a_in - h_a(1);
 					h_b_delta(HX_slices) = h_b_in - h_b(HX_slices);
 	            else
     				h_a_delta(i) = h_a(i - 1) - h_a(i);
@@ -161,9 +165,9 @@ function [T_a_sol, T_b_sol, T_w_sol] = RN_04
             
             % final equations 
 			for i = 1 : HX_slices
-				F_a(i) = -(u_a(i) - u_a_prev(i)) * M / t_delta + (m * h_a_delta(i) + Q_cond_aw(i));
-				F_b(i) = -(u_b(i) - u_b_prev(i)) * M / t_delta + (m * h_b_delta(i) + Q_cond_bw(i));
-                F_w(i) = -(T_w(i) - T_w_prev(i)) * M_w / t_delta + (Q_cond_aw(i) + Q_cond_bw(i)) / cp_w(i) ;
+				F_a(i) = - u_a_delta(i) * M / t_delta + m * h_a_delta(i) + Q_cond_aw(i);
+				F_b(i) = - u_b_delta(i) * M / t_delta + m * h_b_delta(i) + Q_cond_bw(i);
+                F_w(i) = - T_w_delta(i) * M_w / t_delta + (Q_cond_aw(i) + Q_cond_bw(i)) / cp_w(i) ;
             end
             
 			% combine final exit vector
